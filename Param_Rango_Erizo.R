8275b273f70c79add49ed56f7a57024b02c41489
María José <37712089+mariazuba@users.noreply.github.com>
2020-11-25 10:59:53 -0300

    rangos de parámetros de crecimiento y mortalidad natural (#8)
    * De nuevo cambie color
    
    * parametros para Escenarios de Loo y M
    
    Co-authored-by: Mauricio mardones <mauriciomardones@Mauricios-MacBook-Air.local>

---
+++ Param_Rango_Erizo.R
@@ -0,0 +1,14 @@
+
+
+#Rango de Parametros por zona de L00
+
+parLooXN <- seq(128, 136, 0.5)
+parLooXS <- seq(116, 128, 0.5)
+parLooXI <- seq(128, 136, 0.5)
+
+
+# Rango de Parametros ppr zona para M
+
+parMXN <- seq(0.15, 0.25, 0.01)
+parMXS <- seq(0.22, 0.32, 0.01)
+parMXI <- seq(0.15, 0.25, 0.01)
